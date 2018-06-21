32bbda27a80eb760b729def41206739779e4cf28
1anchen <svgyc@hotmail.co.uk>
2018-06-21 01:11:23 +0100

    uppercase done

--- upper_caser/upper_caser.js
+++ upper_caser/upper_caser.js
@@ -1,8 +1,10 @@
 const UpperCaser = function (words) {
+  this.words = words;
 }
 
 UpperCaser.prototype.toUpperCase = function () {
-
+  let uppercaseArray = this.words.map((word) => word.toUpperCase());
+  return uppercaseArray;
 }
 
 module.exports = UpperCaser;
